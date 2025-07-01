2a311cb57ccf56eb710d1f491a8300da2ad72dba
Branislav Đalić <branislav.djalic@gmail.com>
2025-07-01 20:46:12 +0200

    Update starter.js

--- resources/examples/starter.js
+++ resources/examples/starter.js
@@ -1,9 +1,8 @@
-/* 
-To run a line of code, you can do the following:
-1. Place your cursor in the line you'd like to execute and hit `Control+Enter`.
-2. Select the entire line of code with your mouse or keyboard shortcuts and hit `Control+Enter`.
-3. Hit `Alt+Enter` to execute block of code.
-*/
+ 
+// To run a line of code, you can do the following:
+// 1. Place your cursor in the line you'd like to execute and hit `Control+Enter`.
+// 2. Select the entire line of code with your mouse or keyboard shortcuts and hit `Control+Enter`.
+// 3. Hit `Alt+Enter` to execute block of code.
 
 Clock.bpm = 140
  
@@ -12,3 +11,5 @@ bass = Monosynth('bass.stab')
     gen( beats(8) * 4 ), 
     Euclid(5,16) 
   )
+
+k = Kick().trigger.seq( 1, 1/4 )
