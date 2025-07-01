d460bd9a176b24f398f5f08d5a886855ca35d154
Branislav Đalić <branislav.djalic@gmail.com>
2025-07-01 21:05:08 +0200

    Spacing fixes

--- resources/bundle.js
+++ resources/bundle.js
@@ -84242,8 +84242,7 @@ module.exports = function( Gibber, element = '#editor', userEditable=true ) {
 //     8
 //   )`
 
-const defaultCode = 
-`// To run a line of code, you can do the following:
+const defaultCode = `// To run a line of code, you can do the following:
 // 1. Place your cursor in the line you'd like to execute and hit Control+Enter
 // 2. Select the entire line of code with your mouse or keyboard shortcuts and hit Control+Enter
 // 3. Hit Alt+Enter to execute block of code.
@@ -84256,8 +84255,7 @@ bass = Monosynth('bass.stab')
     Euclid(5,16) 
   )
 
-k = Kick().trigger.seq( 1, 1/4 )
-`
+k = Kick().trigger.seq( 1, 1/4 )`
 
     if( window.location.search !== '' ) {
       // use slice to get rid of ?
