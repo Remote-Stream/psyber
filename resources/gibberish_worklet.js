f760e847300ad8aabdfb8b1dde37fdd0cdf22b7a
Branislav Đalić <branislav.djalic@gmail.com>
2025-07-04 08:50:07 +0200

    MIDI out UI fixes

--- resources/gibberish_worklet.js
+++ resources/gibberish_worklet.js
@@ -11143,7 +11143,7 @@ Pattern.prototype.pianoroll = function ({
   return this;
 };
 
-logger("\uD83C\uDF00 @strudel.cycles/core loaded \uD83C\uDF00");
+//logger("\uD83C\uDF00 @strudel.cycles/core loaded \uD83C\uDF00");
 
 if (globalThis._strudelLoaded) {
   console.warn(`@strudel.cycles/core was loaded more than once...
