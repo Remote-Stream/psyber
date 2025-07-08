660d31f32fdf3504e98987d0dec75f70196b0dcb
Branislav Đalić <branislav.djalic@gmail.com>
2025-07-09 01:52:24 +0200

    codemirror fixes and cleanup

--- resources/examples/demosong.js
+++ resources/examples/demosong.js
@@ -2,31 +2,23 @@ Clock.bpm = 120
 Theory.root = 'c5'
 Theory.mode = 'aeolian'
 
-s = Synth("bleep").fx.add(Reverb())
-s.note.seq(sine(btof(7.6),7,0), Euclid(5,16))
+s = Synth("bleep").fx.add(Reverb() )
+    .note.seq( sine( btof(7.6),7,0 ), Euclid(5, 16) )
 
 k = Kick()
-k.notef.seq(70, 1/4)
+    .notef.seq( 70, 1/4 )
 
 h = Hat()
-h.trigger.seq(sine(3,0.6,0.05) , [1/16,1/8].rnd())
-h.tune.seq(Rndf(0.5,0.6), 1/16)
+    .trigger.seq( sine(3, 0.6, 0.05) , [1/16, 1/8].rnd() )
+    .tune.seq( Rndf(0.5, 0.6), 1/16)
 
-cl = Clap().fx.add(Reverb())
-cl.trigger.seq(0.6,1,0,1/4)
+cl = Clap().fx.add( Reverb() )
+    .trigger.seq(0.6, 1, 0, 1/4)
 
-c = Clave().fx.add(Reverb())
-c.trigger.seq(sine(0.2,.1,.4), Euclid(6,16))
-c.note.seq(sine(5.01,4,5), Euclid(6,16))
+c = Clave().fx.add( Reverb() )
+    .trigger.seq( sine(0.2, 0.1, 0.4), Euclid(6, 16) )
+    .note.seq( sine(5.01, 4,5), Euclid(6, 16) )
 
 b = FM('bass')
-b.note.seq(-7,1/16)
-b.index.seq([2,3,4,5,6],1/16)
-
-// stop all
-s.stop() 
-k.stop()
-h.stop()
-c.stop()
-b.stop()
-cl.stop()
+    .note.seq(-7, 1/16)
+    .index.seq( [2,3,4,5,6], 1/16)
--- resources/examples/starter.js
+++ resources/examples/starter.js
@@ -5,6 +5,7 @@ Execute line of code:    CTRL+ENTER
 Execute block of code:   ALT+ENTER
 Execute page code:       CTRL+A then CTRL+ENTER
 Stop all:                CTRL+.
+Stop all immediately:    SHIFT+CTRL+.
 Save code:               SHIFT+CTRL+S
 Load code:               SHIFT+CTRL+S
 Zoom in:                 SHIFT+CTRL++
@@ -15,42 +16,37 @@ Toggle code visibility:  CTRL+H
 */
 
 // execute this line of code with CTRL+ENTER
-
 kick = Kick().trigger.seq( 1, 1/4 )
 
 // execute this block of code with ALT+ENTER
-
 hat = Hat({ gain:.075 })
 hat.trigger.seq( [1,.5], [1/8, 1/16] )
 hat.decay = gen( .02 + cycle( beats(16) * 4 ) * .0125 )
 hat.fx.add( Distortion({ pregain:100, postgain:.1 }) )
 
 // execute this block of code with ALT+ENTER
-
 bass = Monosynth('bass.stab')
-.note.seq( 
-  gen( beats(8) * 4 )  , 
-  Euclid(5,16) 
-)
+  .note.seq( 
+    gen( beats(8) * 4 ), 
+    Euclid(5,16) 
+  )
 
 // execute this block of code with ALT+ENTER
 // then press SHIFT+CTRL+C to toggle free camera mode (use WASD and arrow keys to direct camera)
-
 material = Material( 'phong', Vec3(.05), Vec3(.5), Vec3(1), 8, Vec3(1,4,1) )
 
 Light( Vec3(-2,2,3), Vec3(1,0,0) )
 Background( Vec3(0) )
 Fog( .5, Vec3(0) )
  
-
 Repeat(
- 	s =  Sphere( .25 ).material( material ),
+ 	s = Sphere( .25 ).material( material ),
   Vec3( .75)
-).render()
+  ).render()
 
 // add some interactivity
 s.radius = kick
 
-//use CTRL+SHIFT+T to toggle camera and start moving around scene with WASD, Alt for speed and arrow keys
+//use CTRL+SHIFT+C to toggle camera and start moving around scene with WASD, Alt for speed and arrow keys
 
 // Press CTRL+. anytime to stop audio and video! Checkout other tutorials!
