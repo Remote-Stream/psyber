e5d178b3c30a3183a0cb3bf70048313b6da1e6b5
Branislav Đalić <branislav.djalic@gmail.com>
2025-07-11 03:40:02 +0200

    add background image tutorials

---
+++ resources/examples/backgrounds.js
@@ -0,0 +1,15 @@
+// it is easy to set custom image as a background in your Dilber editor
+
+// try this one from my favorite move
+bcg('https://images.kinorium.com/movie/shot/135141/w1500_199042.jpg')
+
+// to see code better with background use CTRL+ALT+B to turn on code background
+bcg('https://images.kinorium.com/movie/shot/135141/w1500_199049.jpg')
+
+// matrix background
+bcg('https://www.looper.com/img/gallery/15-movies-about-hackers-you-should-try-next/intro-1650898036.webp')
+
+// to go back to normal mode and remove background
+bcg()
+
+// dilber also remembers your last background between sessions so it wont get lost after a reload
